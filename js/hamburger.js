b805633a4943f8f3bd1680db3cb37bc645c9eac7
Hiram Ramírez <68158101+HiramRamirezR@users.noreply.github.com>
2025-08-16 03:17:56 -0600

    implement hamburguer menu for mobile version

---
+++ js/hamburger.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburgerMenu = document.querySelector('.hamburger-menu');
+    const mainNav = document.querySelector('.main-nav');
+
+    if (hamburgerMenu && mainNav) {
+        hamburgerMenu.addEventListener('click', () => {
+            mainNav.classList.toggle('show-menu');
+        });
+    }
+});
